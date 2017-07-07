f047dd56f096286bdedc7a2c8a791e2dc9217a85
al falah taieb <mohamedtaieb.alfalah@esprit.tn>
2017-07-07 13:56:44 +0200

    Clock Working with some Colors over the hand

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+const secondHand=document.querySelector('.second-hand')
+const minHand=document.querySelector('.min-hand')
+const hourHand=document.querySelector('.hour-hand')
 function sdate() {
 	// console.log('Hi you :3')
 
@@ -5,11 +9,17 @@ function sdate() {
 	const seconds = now.getSeconds();
 	const minutes = now.getMinutes();
 	const hours = now.getHours();
-	console.log(hours);
-	console.log(minutes);
-	console.log(seconds);
-	
+	const secondsDeg=((seconds/60) * 360)+ 90;
+	const minutesDeg=((minutes/60) * 360)+ 90;
+	const hoursDeg=((hours/12))+90;
 
+	// console.log(hours);
+	// console.log(minutes);
+	// console.log(seconds);
+	secondHand.style.transform=`rotate(${secondsDeg}deg)`;
+	minHand.style.transform=`rotate(${minutesDeg}deg)`;
+	hourHand.style.transform=`rotate(${hoursDeg}deg)`;
 }
+
 
 setInterval(sdate, 1000)//message chaque  1 sc
